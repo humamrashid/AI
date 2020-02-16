a41c655678a652f5146739e998b007c492770e58
Humam Rashid <humamrashid@gmail.com>
2020-02-16 03:03:41 -0500

    Initial commit.

---
+++ farmer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+# Implementation of farmer, fox, goose and grain problem solution.
+
+# parameters: tuple(location, status)
+# returns: string(action)
+def reflex_farmer_agent(locstat):
+    print(locstat[0], locstat[1])
+
+print(reflex_farmer_agent(("A", 1)))
+
+# EOF.

df445c83486c77f296637faea7ee5f0153bf915e
Humam Rashid <humamrashid@gmail.com>
2020-02-19 14:51:46 -0500

    Inc. commit.

--- farmer.py
+++ farmer.py
@@ -5,38 +5,36 @@
 # River banks: A is the starting point, B is the destination.
 
 # States:
-# (Farmer = Fa, Fox = Fo, Goose = Go, Grain = Gr)
+# (Frrmer = Fr, Fox = Fo, Goose = Go, Grain = Gr)
 #
-# 0  = (Fa, Fo, Go, Gr) on A, () on  B  (Valid)
-# 1  = (Fa, Fo, Go) on A, (Gr) on B     (Valid)
-# 2  = (Fa, Fo, Gr) on A, (Go) on B     (Valid)
-# 3  = (Fa, Gr, Go) on A, (Fo) on B     (Valid)
-# 4  = (Fo, Gr, Go) on A, (Fa) on B     (Invalid)
-# 5  = (Fa, Fo) on A, (Go, Gr) on B     (Invalid)
-# 6  = (Fa, Gr) on A, (Fo, Go) on B     (Invalid)
-# 7  = (Fa, Go) on A, (Fo, Gr) on B     (Valid)
-# 8  = (Fo, Gr) on A, (Fa, Go) on B     (Valid)
-# 9  = (Fo, Go) on A, (Fa, Gr) on B     (Invalid)
-# 10 = (Gr, Go) on A, (Fa, Fo) on B     (Invalid)
-# 11 = (Fa) on A, (Fo, Gr, Go) on B     (Invalid)
-# 12 = (Fo) on A, (Fa, Gr, Go) on B     (Valid)
-# 13 = (Gr) on A, (Fa, Fo, Go) on B     (Valid)
-# 14 = (Go) on A, (Fa, Fo, Gr) on B     (Valid)
-# 15 = () on A, (Fa, Fo, Gr, Go) on B   (Valid)
+# 0  = (Fr, Fo, Go, Gr) on A, () on  B  (Valid)
+# 1  = (Fr, Fo, Go) on A, (Gr) on B     (Valid)
+# 2  = (Fr, Fo, Gr) on A, (Go) on B     (Valid)
+# 3  = (Fr, Gr, Go) on A, (Fo) on B     (Valid)
+# 4  = (Fo, Gr, Go) on A, (Fr) on B     (Invalid)
+# 5  = (Fr, Fo) on A, (Go, Gr) on B     (Invalid)
+# 6  = (Fr, Gr) on A, (Fo, Go) on B     (Invalid)
+# 7  = (Fr, Go) on A, (Fo, Gr) on B     (Valid)
+# 8  = (Fo, Gr) on A, (Fr, Go) on B     (Valid)
+# 9  = (Fo, Go) on A, (Fr, Gr) on B     (Invalid)
+# 10 = (Gr, Go) on A, (Fr, Fo) on B     (Invalid)
+# 11 = (Fr) on A, (Fo, Gr, Go) on B     (Invalid)
+# 12 = (Fo) on A, (Fr, Gr, Go) on B     (Valid)
+# 13 = (Gr) on A, (Fr, Fo, Go) on B     (Valid)
+# 14 = (Go) on A, (Fr, Fo, Gr) on B     (Valid)
+# 15 = () on A, (Fr, Fo, Gr, Go) on B   (Valid)
 
 on_boat = {
-        0: ("Farmer"),
-        1: ("Farmer", "Goose"),
-        2: ("Farmer", "Grain"),
-        3: ("Farmer", "Fox"),
+        "fr": ("Farmer"),
+        "frgo": ("Farmer", "Goose"),
+        "frgr": ("Farmer", "Grain"),
+        "frfo": ("Farmer", "Fox"),
         }
 
 def take(bank, group):
     print(f"Take {group} to {bank}")
 
-# parameters: location
-# returns:    action
-def reflex_farmer_agent(location):
+def reflex_farmer_agent(state):
     if location == "A":
         take(location, on_boat[1])
 

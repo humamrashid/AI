67bf802ffeb2d1e1ee4be1308f2ac6f4bbcbfe7a
Humam Rashid <humamrashid@gmail.com>
2020-03-03 23:35:07 -0500

    Inc. commit.

--- 8puzzle.py
+++ 8puzzle.py
@@ -2,6 +2,22 @@
 
 # Solution for 8-puzzle.
 
+import numpy as np
 
+# 8-puzzle is represented as a 1d array, initialized with default values representing the initial
+# state (numbered tiles). A value of 0 indicates the blank tile.
+puzzle = np.array([(1,4,3), (7,8,0), (6,2,5)])
+
+def print_puzzle():
+    for r in puzzle:
+        print('*******************')
+        for c in r:
+            print('| ', end=" ")
+            print(f'{c}  ', end="")
+            #print(' |', end="")
+        print('| ')
+    print('*******************')
+
+print_puzzle()
 
 # EOF.

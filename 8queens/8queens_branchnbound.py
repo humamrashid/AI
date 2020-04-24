d40b96091c69daf2507bfb07c509551b99c31567
Humam Rashid <humamrashid@gmail.com>
2020-04-24 04:19:33 -0400

    Inc. commit.

--- 8queens/8queens_branchnbound.py
+++ 8queens/8queens_branchnbound.py
@@ -2,3 +2,17 @@
 # Author: Humam Rashid
 
 # Solution for 8-queens problem using branch and bound method.
+
+import numpy as np
+
+# Ask user for board size.
+print("Enter N for NxN board: ", end="")
+N = int(input())
+
+board_matrix = np.zeros((N, N))
+right_diag_matrix = np.empty((N, N))
+left_diag_matrix = np.empty((N, N))
+
+print(board_matrix)
+
+# EOF.

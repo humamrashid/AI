39cc3e9edeb079d4b7ef3ae92f6d6b5f60355d85
Humam Rashid <humamrashid@gmail.com>
2020-05-10 04:28:06 -0400

    Added labels.

--- machine_learning/sinusoidal_data.py
+++ machine_learning/sinusoidal_data.py
@@ -6,22 +6,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 from numpy.polynomial import polynomial as pnl
 
-# Original sinusodial curve sine(2*pi*x) with 100 values from [0, 1].
-original = np.linspace(0, 1, 100)
+# Constants.
+ORIG_N = 100
+DATA_N = 10
+PLN_DEG = 0
 
-# Smaller sample of 10 values from [0, 1] with Gaussian white noise. Standard deviation of 0.05 used
-# to keep amount of noise "small."
-x = np.linspace(0, 1, 40)
-x_noise = np.random.normal(loc=x, scale=0.05, size=40)
+# Original sinusodial curve sine(2*pi*x) of ORIG_N values from [0, 1].
+original = np.linspace(0, 1, ORIG_N)
+
+# Smaller sample of DATA_N values from [0, 1].
+x = np.linspace(0, 1, DATA_N)
+
+# Training data with small amount of Gaussian white noise.
+t_data = np.random.normal(loc=x, scale=0.05, size=DATA_N)
 
 # Least squares fitting.
-c = pnl.polyfit(x, np.sin(2*np.pi*x_noise), 15)
-ffit = pnl.polyval(x, c)
+coeff = pnl.polyfit(x, np.sin(2*np.pi*t_data), PLN_DEG)
+ffit = pnl.polyval(x, coeff)
 
 # Plot original sinusoidal, scatter plot of data points and least squares fit.
-plt.plot(original, np.sin(2*np.pi*original), color='green')
-plt.scatter(x, np.sin(2*np.pi*x_noise), color='blue')
-plt.plot(x, ffit, color='red')
+plt.plot(original, np.sin(2 * np.pi * original), color='green')
+plt.scatter(x, np.sin(2 * np.pi * t_data), color='blue', label=f"N = {DATA_N}")
+plt.plot(x, ffit, color='red', label=f"M = {PLN_DEG}")
 
 # Viewing setttings.
 plt.xlabel('x')
@@ -29,6 +35,7 @@ plt.ylabel('t')
 plt.axis('tight')
 plt.xlim(0, 1)
 plt.ylim(-1, 1)
+plt.legend()
 plt.show()
 
 # EOF.

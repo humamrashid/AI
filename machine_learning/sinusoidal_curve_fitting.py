562bf676551a62a2e1138fe72493271b454b6e0c
Humam Rashid <humamrashid@gmail.com>
2020-05-11 05:48:40 -0400

    renamed file

--- machine_learning/sinusoidal_curve_fitting.py
+++ machine_learning/sinusoidal_curve_fitting.py
@@ -16,7 +16,7 @@ pln_deg = int(sys.argv[2])
 orig_n = int(sys.argv[3])
 if data_n <= 0 or orig_n <= 0:
     print("Number of data points must be > 0")
-    exit
+    exit(1)
 if pln_deg < 0:
     print("Polynomial degree must be >= 0")
     exit(1)
@@ -28,15 +28,15 @@ original = np.linspace(0, 1, orig_n)
 x = np.linspace(0, 1, data_n)
 
 # Training data with small amount of Gaussian white noise.
-t_data = np.random.normal(loc=x, scale=0.05, size=data_n)
+t_data = np.random.normal(loc=np.sin(2 * np.pi * x), scale=0.2, size=data_n)
 
 # Least squares fitting.
-coeff = pnl.polyfit(x, np.sin(2 * np.pi * t_data), pln_deg)
+coeff = pnl.polyfit(x, t_data, pln_deg)
 ffit = pnl.polyval(x, coeff)
 
 # Plot original sinusoidal, scatter plot of data points and least squares fit.
 plt.plot(original, np.sin(2 * np.pi * original), color='green')
-plt.scatter(x, np.sin(2 * np.pi * t_data), color='blue', label=f"N = {data_n}")
+plt.scatter(x, t_data, color='blue', label=f"N = {data_n}")
 plt.plot(x, ffit, color='red', label=f"M = {pln_deg}")
 
 # Viewing setttings.
